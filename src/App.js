122701119de7e5c8ef6d34dd41fe897ca33a9af5
Xavier Ruiz <69017482+FHN-Kodiris@users.noreply.github.com>
2020-10-26 13:51:31 -0500

    Drag and Drop Compatibility

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import Review from './components/Review'
 import HeroList from './components/Hero/HeroList'
 import HeroSelect from './components/CreateViews/HeroSelect/HeroSelect'
 import Signin from './Signin/Signin'
+import Details from './components/CreateViews/Details/Details'
+import Aspects from './components/CreateViews/Details/Aspects/Aspects'
+import Compatibility from './components/CreateViews/Details/Compatibility/Compatibility'
+import Abilities from './components/CreateViews/Details/Abilities/Abilities'
 
 function App() {
   const getHeroList = (heroListObj) => {
@@ -28,6 +32,10 @@ function App() {
         <Route component={BrowsePage} path="/browse" />
         <Route component={Review} path="/create/review" />
         <Route component={Customize} path="/create/customize" />
+        <Route component={Abilities} path="/create/details/abilities" />
+        <Route component={Compatibility} path="/create/details/compatibility" />
+        <Route component={Aspects} path="/create/details/aspects" />
+        <Route component={Details} path="/create/details" />
         <Route component={HeroSelect} path="/create/heroselect" />
         <Route component={Create} path="/create" />
         <Route component={Home} path="/" />
